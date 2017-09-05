c0ccc037804f812df4cc380b3a156d238df7e564
张小娟 <15527436800@qq.com>
2017-09-05 11:35:08 +0800

    富文本框细节调整

--- src/main/java/com/yijiupi/himalaya/base/webcontrol/ueditor/FileUploadBaseController.java
+++ src/main/java/com/yijiupi/himalaya/base/webcontrol/ueditor/FileUploadBaseController.java
@@ -44,12 +44,14 @@ public class FileUploadBaseController {
 	 */
 	@RequestMapping(value = "/common/uploadFile")
 	public BaseResult uploadFile(@RequestParam(value="action",required = false) String action,@RequestParam(value="noCache",required = false) String noCache, MultipartFile image) {
-		
+		if(image == null){
+			return new ROResult<FileInfoRO>(new FileInfoRO());
+		}
 		FileConfig fileConfig = iFileService.getFileConfig(PicCategory.Banner);
 		FileInfoDTO fileInfoDTO = FileUpdateHelper.uploadPicFile(image, "1", fileConfig);
 	    com.yijiupi.himalaya.basic.file.domain.FileInfo fileInfoModel = iFileService.saveFile(fileInfoDTO);
 	    FileInfoRO ro = FileInfoConverter.converterRO(fileInfoModel);
-		ROResult<FileInfoRO> result = new ROResult<FileInfoRO>(ro);
+	    ROResult<FileInfoRO> result = new ROResult<FileInfoRO>(ro);
 		return result;
 	}
 
