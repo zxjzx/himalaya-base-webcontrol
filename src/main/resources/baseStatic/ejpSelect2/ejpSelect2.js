86f47f8ae2f8fb16e8d7b9ed587da9168790aca8
lipan <lipan@JAVADEVELOPER44.yjplan.net>
2017-10-18 17:33:09 +0800

    ejp-select2指令。

--- src/main/resources/baseStatic/ejpSelect2/ejpSelect2.js
+++ src/main/resources/baseStatic/ejpSelect2/ejpSelect2.js
@@ -1,6 +1,11 @@
 /**
  * select2下拉框的单选or多选
+ * <select class="form-control" ng-model="vo.shopId" ejp-select2 id="dealer" multiple
+		ng-options = "optionItem.id as optionItem.name for optionItem in allDealerList"></select>
+ * 必填 ： ejp-select2 id=""(任意定义值) 
+ * 选填 ：multiple(填写则表示多选，得到的数据为[id集合],不填，则默认单选)。 
  * 
+ * 清空选中值:在调用指令的js中,在清空的函数中调用这句。$("#dealer").val("").trigger("change");
  */
 (function(){
 	'use strict' ;
@@ -16,8 +21,8 @@
 				if(attr.multiple){
 					element.css('border','1px solid #ddd') ;
 				}
-				var idName = attr.id ;
-				$('#'+idName).select2({
+				var idCtrl = $('#'+ attr.id );
+				idCtrl.select2({
 					placeholder : "请选择" ,
 					allowClear : true ,
 				});
