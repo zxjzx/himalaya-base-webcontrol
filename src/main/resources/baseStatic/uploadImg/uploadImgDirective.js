63fd3a9ffdd3ceab5d065e96707eb225eae20d29
张小娟 <15527436800@qq.com>
2017-10-30 16:12:01 +0800

    上传多张图片

--- src/main/resources/baseStatic/uploadImg/uploadImgDirective.js
+++ src/main/resources/baseStatic/uploadImg/uploadImgDirective.js
@@ -10,28 +10,48 @@
 			restrict:'AE',
 			scope:{
 				uploaderName:'@',
-				picList:'=',
-				id:'='
+				picList:'=',//已上传成功的用以显示的图片数组
+				id:'=',
+				queueLimit:'@',
+				setDefault:'='//图片上传成功后执行的操作
+				
 			},
 			templateUrl:'../baseStatic/uploadImg/uploadImgMuti.html',
-			controller : ['$scope','getUserInfo','FileUploader','ejpAlert','$http',function($scope,getUserInfo,FileUploader,ejpAlert,$http){
+			controller : ['$scope','getUserInfo','FileUploader','ejpAlert','$http','picFilter','fileReader',
+			              function($scope,getUserInfo,FileUploader,ejpAlert,$http,picFilter,fileReader){
 				
 				var uploader = $scope.uploader = new FileUploader({
 					url: 'common/uploadFile/' + $scope.id + '/0/' + getUserInfo.userInfo().userId + '.action',
+					removeAfterUpload: 'true',
+//					queueLimit:$scope.queueLimit?$scope.queueLimit:10,//最多同时上传的文件个数,如果不设置默认为1
+					// 限制文件大小为100k
+	    			filters : [ picFilter.sizeFilter ]
 				});
 				$scope.uploaderName = $scope.uploader;
-				console.log($scope.picList);
+			
 				
 				//上传预览成功
 				uploader.onAfterAddingFile = function (fileItem) {
-		            console.info('onAfterAddingFile', fileItem);
+		            fileReader.readAsDataUrl(fileItem._file, $scope).then(function(result) {
+	                    var img = new Image();
+	                    img.onload=function(){
+	                    	if (this.width>400 || this.height>400){//获取上传图片的宽高
+	                    		console.log("上传图片的宽高建议为400*400！")
+	                    	}
+	                    };
+	                    img.src = result;
+	                }); 
+		            
 		        };
 		        
 		        //上传成功
 				uploader.onSuccessItem = function (fileItem, response, status, headers) {
-		            /*if (response.result == 'success') {
+		            if (response.result == 'success') {
 		                $scope.picList.push(response.data);
-		            }*/
+		                if($scope.picList.length === 1){//设置默认图片
+		                	$scope.setDefault($scope.picList);
+		                }
+		            }
 				};
 				
 				//删除图片
@@ -51,10 +71,61 @@
 		        		//点击了取消按钮
 		        	})
 		        };
-				
-				
 			}]
 			
 		}
 	} ])
+	//file upload 限制图片大小和格式
+	.service("picFilter",['ejpAlert',function(ejpAlert){
+		this.sizeFilter = {
+				name:"imgSizeFilter",
+				fn:function(item){
+					// 图片大小为100k
+					var IMAGE_SIZE = 100 * 1024;
+					if(item.size > IMAGE_SIZE){
+						ejpAlert.show('活动图片最大为100k');
+						return false;
+					}
+					return item.size <= IMAGE_SIZE;
+				}
+		};
+	}])
+	.factory('fileReader', ["$q", "$log", function($q, $log){  
+        var onLoad = function(reader, deferred, scope) {  
+            return function () {  
+                scope.$apply(function () {  
+                    deferred.resolve(reader.result);  
+                });  
+            };  
+        };  
+  
+        var onError = function (reader, deferred, scope) {  
+            return function () {  
+                scope.$apply(function () {  
+                    deferred.reject(reader.result);  
+                });  
+            };  
+        };  
+  
+        var getReader = function(deferred, scope) {  
+            var reader = new FileReader();  
+            reader.onload = onLoad(reader, deferred, scope);  
+            reader.onerror = onError(reader, deferred, scope);  
+            return reader;  
+        };  
+  
+        var readAsDataURL = function (file, scope) {  
+            var deferred = $q.defer();  
+            var reader = getReader(deferred, scope);  
+            reader.readAsDataURL(file);  
+            return deferred.promise;  
+        };  
+  
+        return {  
+            readAsDataUrl: readAsDataURL  
+        };  
+    }])
 })();
+
+
+
