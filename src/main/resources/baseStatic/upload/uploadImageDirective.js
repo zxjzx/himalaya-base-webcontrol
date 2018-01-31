b93c6489b4e438745940159aaf008fb7dcc47155
张小娟 <15527436800@qq.com>
2018-01-31 17:08:33 +0800

    上传图片裁剪功能完成

--- src/main/resources/baseStatic/upload/uploadImageDirective.js
+++ src/main/resources/baseStatic/upload/uploadImageDirective.js
@@ -16,8 +16,7 @@
                     getImgData:'=',//返回给页面关于图片的数据信息
                     imgAmount:'@',//上传图片数量的限制
                     bussinessId:'=',//上传图片所属业务id
-                    imgWidth:'@',
-                    imgHeight:'@'
+                    imgSize:'='//图片尺寸
                 },
                 templateUrl:'../baseStatic/upload/preViewImage.html',
                 controller:['$scope','getUserInfo','$http','$modal',function($scope,getUserInfo,$http,$modal){
@@ -46,13 +45,16 @@
                     var userId = getUserInfo.userInfo().userId;
                     var bussinessId = $scope.bussinessId;
 
-                    //上传操作
+                    //上传到后台的操作
                     $scope.getPicInfo = function(dataList){
                         var image = new FormData();
                         //dataList需要上传到后台的数据
                         dataList.forEach(function (item) {
                             image.append('files',item.image);
                         });
+
+                        // image.append('files',fileBlob,'image.png')
+
                         $http({
                             method:"post",    　　// 可以是get,post,put, delete,head,jsonp;常使用的是get,post
                             url:'basewebcontrol/upload/common/uploadFiles/'+bussinessId+'/0/'+userId, 　　  //请求路径
@@ -67,33 +69,58 @@
 
                     //双击编辑图片
                     /**
-                     * TODO
                      * @param item
                      */
+                    var fileBlob;
                     $scope.editImg = function (item) {
+                        var index = this.$index;
                         $modal.open({
                             templateUrl: '../baseStatic/upload/editImageModal.html',
                             controller: editImageModalController,
                             size: 'lg',
                             resolve: {
                                 requestResults: function () {
-                                    return item;
+                                    return {
+                                        item:item,
+                                        imgSize:$scope.imgSize//图片尺寸
+                                    };
                                 }
                             }
+                        }).result.then(function (resultImg) {
+                            if(resultImg){
+                                fileBlob = dataURLtoBlob(resultImg);
+                                if(fileBlob){
+                                    $scope.canvasList[index].image = fileBlob;
+                                    $scope.canvasList[index].resultImg = resultImg;
+                                }
+
+                            }
+
+
                         })
+                    };
+
+                    // base64编码转为file
+                    function dataURLtoBlob(dataurl) {
+                        var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+                            bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+                        while(n--){
+                            u8arr[n] = bstr.charCodeAt(n);
+                        }
+                        return new Blob([u8arr], {type:mime});
                     }
 
                     function editImageModalController(requestResults,$scope,$modalInstance) {
 
-                        $scope.imgInfo = requestResults;
-                        // console.log($scope.imgInfo);
+                        $scope.imgInfo = requestResults.item;
+                        $scope.imgSize = requestResults.imgSize;
                         $scope.ok = function() {
-                            $modalInstance.close();
+                            //返回经过处理的图片
+                            $modalInstance.close($scope.resultImg);
                         };
                         // 取消按钮
                         $scope.cancel = function() {
                             // 跳转到列表页面
-                            //$location.path('/opUser_list.html');
                             $modalInstance.dismiss('cancel');
                         };
                     }
@@ -102,6 +129,7 @@
                 }]
             }
         }])
+
 
     //点击上传操作
         .directive('fileModel', function ($parse) {/*$parse是AngularJS的内置directive*/
@@ -115,26 +143,32 @@
                 }
             };
         })
-        //显示
+        //预览上传的图片
         .directive('ngShowCanvas',[function(){
             return {
-                restrict: 'A',
+                restrict: 'AE',
+                scope:{
+                    triggerFun:'&',//不满足格式要求的图片触发函数
+                    fileInfo:'='
+                },
                 template: '<canvas/>',
-                controller:['$scope',function($scope){
-                }],
                 link: function(scope, element, attributes) {
                     //图片要求尺寸
-                    var imgWidth = scope.imgWidth;
-                    var imgHeight = scope.imgHeight;
+                    var imgWidth = 400;
+                    var imgHeight = 300;
 
-                    var params = scope.$eval(attributes.ngShowCanvas);
-                    if(!params.file){
+                    var params = scope.fileInfo;
+
+                    if(!params){
                         return;
                     }
+                    params.width = 400;
+
                     var canvas = element.find('canvas');
+
                     var reader = new FileReader();
                     reader.onload = onLoadFile;
-                    reader.readAsDataURL(params.file);
+                    reader.readAsDataURL(params);
                     function onLoadFile(event) {
                         var img = new Image();
                         img.onload = onLoadImage;
@@ -144,7 +178,8 @@
                         //this.width,this.height 图片的原始尺寸-宽度和高度
                         if((imgWidth && (imgWidth != this.width)) || (imgHeight && imgHeight != this.height)) {
                             console.log("您上传的图片尺寸为"+this.width+"X"+this.height+";此处图片格式要求为"+imgWidth+'X'+imgHeight);
-                            scope.unsatisfySize = true;
+                            scope.triggerFun();
+                            return;
                         }
                         var width = params.width || this.width / this.height * params.height;
                         var height = params.height || this.height / this.width * params.width;
@@ -152,7 +187,7 @@
                         var scale = this.width / this.height;
                         if(width>800 || height>500){
                             if(height*scale>width){
-                                height = 500,
+                                height = 500;
                                 width = 500*scale;
                             }else{
                                 width = 800;
@@ -160,10 +195,166 @@
                             }
                         }
                         canvas.attr({ width: width, height: height });
+                        canvas[0].getContext('2d').clearRect(this, 0, 0, width, height);
                         canvas[0].getContext('2d').drawImage(this, 0, 0, width, height);
                     }
                 },
 
+            }
+        }])
+
+        //在弹窗中编辑图片
+        .directive('editImgDirect',[function () {
+            return {
+                restrict:'AE',
+                scope:{
+                    imgInfo:'=',//点击上传时页面传递过来的原始图片信息
+                    imgSize:'=',//目标尺寸，即阴影部分的宽高
+                    resultImg:'='//返回的已处理（裁剪）图片
+                },
+                templateUrl:'../baseStatic/upload/html/editDiv.html',
+                controller:['$scope',function ($scope) {
+                }],
+                link : function (scope,element,attribute,ctrl) {
+
+                    //新建canvas
+                    var newCanvas =  document.createElement("canvas");
+                    angular.element("#mydiv").append(angular.element(newCanvas));
+
+                    var zr = zrender.init(newCanvas,{width:800,height:500});
+                    var img = new Image();
+                    var reader = new FileReader();
+                    reader.onload = function (event) {
+                        img.onload = onLoadImage;
+                        img.src = event.target.result;
+                    };
+                    reader.readAsDataURL(scope.imgInfo);
+
+
+                    var imageInfo;//原图片信息
+                    var reduceScale;//缩小倍数-为了防止过大的图片超出页面范围，因此缩小预览
+                    var backgroundImage;
+
+                    var orinImgWidth,orinImgHeight,orinImgScale;
+                    function onLoadImage() {
+                        //原始图片的大小
+                        var aimW = 800;
+                        var aimH = 500;
+                        var aimScale = aimW/aimH;
+
+                        var width = orinImgWidth = this.width;//原始图片的大小
+                        var height = orinImgHeight =this.height;//原始图片的大小
+
+                        var scale = orinImgScale = this.width / this.height;//比例
+                        if(width>aimW && height<aimH){
+                            width = aimW;
+                            height = width/scale;
+                        }
+                        if(width < aimW && height>aimH){
+                            height = aimH;
+                            width = height*scale;
+                        }
+                        if(width>aimW && height>aimH){
+                            if(scale>aimScale){
+                                width = aimW;
+                                height = width/scale;
+                            }else{
+                                height = aimH;
+                                width = height*scale;
+                            }
+                        }
+
+                        reduceScale = this.width/width;//原始图片/预览图片 = 缩小倍数
+
+                        imageInfo = this;
+                        backgroundImage = new zrender.Image({
+                            position: [0,0],
+                            scale: [1, 1],//缩放比例
+                            style: {
+                                x: 0,
+                                y: 0,
+                                image: this,
+                                width: width,
+                                height: height
+                            },
+                            draggable: false
+                        });
+                        zr.add(backgroundImage);
+                        zr.add(circle);
+                        getImgCut();//初次加载时默认执行的截取图片操作
+                    }
+
+
+                    var circle = new zrender.Rect({
+                        shape: {
+                            x: 0,
+                            y: 0,
+                            width: scope.imgSize[0],
+                            height:scope.imgSize[1]
+                        },
+                        style: {
+                            fill: '#ccc',
+                            stroke: 'white',
+                            lineWidth:'1',
+                            opacity:0.5
+                        },
+                        draggable:true
+                    });
+
+                    var resultCanvas =  document.createElement("canvas");
+                    resultCanvas.style.display = "none";
+                    angular.element("#mydiv").append(angular.element(resultCanvas));
+
+                    circle.on('mouseup',function () {
+                        getImgCut();
+                    });
+
+                    zr.on('mousewheel',function (e) {
+
+                        //e.wheelDelta:鼠标向上滚动为1,下为-1
+                        var num = 20;
+                        orinImgWidth -= num*e.wheelDelta;
+                        orinImgHeight -= num/orinImgScale*e.wheelDelta;
+                        var backCanvas = element.find('canvas')[0];
+                        backCanvas.getContext('2d').clearRect(0,0,800,600);
+                        backCanvas.getContext('2d').drawImage(imageInfo,0,0,img.width,img.height,0,0,orinImgWidth,orinImgHeight);
+                    });
+
+
+
+
+
+                    //进行裁剪具体操作
+                    function getImgCut() {
+                        //为什么要*reduceScale，因为在页面预览的图片的size已经经过扩大或者缩小，不是原始图片的宽高，若要得到选中的区域，必须得到源图片的真实宽高才能得到正确的预览图
+                        var sx = circle.position[0]*reduceScale;//开始剪切的 x 坐标位置
+                        var sy = circle.position[1]*reduceScale;
+                        var sw = scope.imgSize[0]*reduceScale;//被剪切图像的宽度
+                        var sh = scope.imgSize[1]*reduceScale;
+
+                        var cw = scope.imgSize[0];//要使用的图像的宽度。（伸展或缩小图像）
+                        var ch = scope.imgSize[1];
+
+                        //新建canvas显示裁剪后的图片
+                        var canvas = element.find('canvas');
+
+                        canvas[1].width=cw;
+                        canvas[1].height=ch;
+                        canvas[1].getContext('2d').drawImage(imageInfo, sx, sy, sw, sh,0,0,cw,ch);
+
+                        var image = new Image();
+                        image.src = canvas[1].toDataURL("image/png");
+                        image.onload = function () {
+                            scope.resultImg = image.src;
+                            scope.$apply();
+                            // canvas[1].getContext('2d').clearRect(0,0,cw,ch);
+                        };
+                    }
+
+
+
+
+                }
             }
         }])
 
